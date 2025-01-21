3e1d4561279d6291e97d0e0018d64a370c9f2068
hamedwifi45 <rramyt58@gmail.com>
2025-01-21 22:06:10 +0300

    تحديث جديد

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -77,20 +77,10 @@
                         </a>
                         <div class="dropdown-menu dropdown-menu-left px-2 text-right mt-2">
                             <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-                                <div class="pt-2 pb-3 space-y-1">
-                                    <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                                        {{ __('Dashboard') }}
-                                    </x-responsive-nav-link>
-                                </div>
-                        
                                 <!-- Responsive Settings Options -->
                                 <div class="pt-4 pb-1 border-t border-gray-200">
                                     <div class="flex items-center px-4">
-                                        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                            <div class="shrink-0 me-3">
-                                                <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                            </div>
-                                        @endif
+                                        
                         
                                         <div>
                                             <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
